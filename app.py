aec19247e805e2a4f5caf8ba05aeeffafec3c5d4
swig32 <swig32@users.noreply.github.com>
2019-04-21 19:18:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + " likes to fart a lot!!"
+	new_name = str(name) + " likes to fart a lot!!"
 	return render_template('index.html', name=new_name)
 
 
